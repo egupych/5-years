c987d434171e765d083a8ccb0f6d04ff5eff7fb3
Dmitry Egupov <egupovds@gmail.com>
2023-05-29 07:23:33 +0600

    1

--- app.js
+++ app.js
@@ -223,3 +223,18 @@ function setScrollIntoView4Options(top) {
 		behavior: "smooth"
 	});
 }
+
+function showImage(src) {
+	var modal = document.getElementById("modal");
+	var modalImage = document.getElementById("modal-image");
+	modalImage.src = src;
+	modal.style.display = "block";
+  }
+  
+  function hideImage() {
+	var modal = document.getElementById("modal");
+	modal.style.display = "none";
+  }
+  
+
+  
